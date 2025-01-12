7ab8fa96a3e5f9836b11a99c49659370031b7f34
itsbrunodev <brunka215@gmail.com>
2025-01-12 23:06:19 +0100

    Remove hello message on load

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import { defineExtension } from "reactive-vscode";
-import { window, commands } from "vscode";
+import { commands } from "vscode";
 
 import { browseCommand, browseCommandHandler } from "./commands/browse";
 import { searchCommand, searchCommandHandler } from "./commands/search";
 
 const { activate, deactivate } = defineExtension((context) => {
-  window.showInformationMessage("Hello");
-
   context.subscriptions.push(
     commands.registerCommand(browseCommand, browseCommandHandler)
   );
